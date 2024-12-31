83bc2f1cdfea3759a822144bd02c9a9f491f8305
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-31 18:10:42 -0500

    update: improved error handling

--- Blog_API_Backend/prisma/queries/userQueries.js
+++ Blog_API_Backend/prisma/queries/userQueries.js
@@ -1,32 +1,45 @@
 const prisma = require('../prismaClient')
 
 async function getUsers() {
-    const users = await prisma.user.findMany({
-        select: {
-            id: true,
-            email: true,
-            name: true,
-            username: true,
-            createdAt: true,
-            updatedAt: true,
-            role: true
-        }
-    })
-    return users
+    try {
+        const users = await prisma.user.findMany({
+            select: {
+                id: true,
+                email: true,
+                name: true,
+                username: true,
+                createdAt: true,
+                updatedAt: true,
+                role: true
+            }
+        })
+        return users
+    } catch(error) {
+        throw new Error(`Failed to fetch users: ${error.message}`);
+    }
 }
 
 async function postUsers(userData) {
-    const user = await prisma.user.create({
-        data: userData,
-        select: {
-            id: true,
-            email: true,
-            name: true,
-            username: true,
-            createdAt: true,
-            updatedAt: true,
-            role: true
-        }
-    })
-    return user
+    try {
+        const user = await prisma.user.create({
+            data: userData,
+            select: {
+                id: true,
+                email: true,
+                name: true,
+                username: true,
+                createdAt: true,
+                updatedAt: true,
+                role: true
+            }
+        })
+        return user
+    } catch (error) {
+        throw new Error(`Failed to create user: ${error.message}`);
+    }
+}
+
+module.exports = {
+    getUsers,
+    postUsers,
 }
