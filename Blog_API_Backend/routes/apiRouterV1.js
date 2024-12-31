a62d551f267ecfa963713684658565e7d77a6255
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-31 17:18:00 -0500

    add: basic user api routes

--- Blog_API_Backend/routes/apiRouterV1.js
+++ Blog_API_Backend/routes/apiRouterV1.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
-const userController = () => null
+const userController = require('../controllers/userController')
 
 //users
-router.get("/users", userController);
+router.get("/users", userController.getUsers);
+router.get("/users/:userId", userController.getUserById);
+router.post("/users", userController.postUser);
+router.put("/users/:userId", userController.putUser);
+router.delete("/users/:userId", userController.deleteUser);
 
 module.exports = router
