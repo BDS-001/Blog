ad9522d6b6f453d9ae7e48695fd0e1985099ea80
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-01-02 21:42:08 -0500

    add: router handles comments

--- Blog_API_Backend/routes/apiRouterV1.js
+++ Blog_API_Backend/routes/apiRouterV1.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const userController = require('../controllers/userController')
+const commentsController = require('../controllers/commentController')
 
 //users
 router.get("/users", userController.getUsers);
@@ -7,5 +8,13 @@ router.get("/users/:userId", userController.getUserById);
 router.post("/users", userController.postUser);
 router.put("/users/:userId", userController.putUser);
 router.delete("/users/:userId", userController.deleteUser);
+
+//comments
+router.get("/comments", commentsController.getComments);
+router.get("/blogs/:blogId/comments", commentsController.getBlogComments);
+router.get("/comments/:commentId", commentsController.getCommentById);
+router.post("/comments", commentsController.postComment);
+router.put("/comments/:commentId", commentsController.putComment);
+router.delete("/comments/:commentId", commentsController.deleteComment);
 
 module.exports = router
