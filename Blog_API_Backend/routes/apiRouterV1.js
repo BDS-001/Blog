2ef72974c3aa610f520ba1830d5aa68dcdda60d3
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-01-07 18:57:15 -0500

    add: blog api routes setup

--- Blog_API_Backend/routes/apiRouterV1.js
+++ Blog_API_Backend/routes/apiRouterV1.js
@@ -1,23 +1,29 @@
 const router = require("express").Router();
 const userController = require('../controllers/userController')
 const commentsController = require('../controllers/commentController')
+const blogController = require('../controllers/blogController')
 
 //users
 router.get("/users", userController.getUsers);
 router.get("/users/:userId", userController.getUserById);
-router.post("/users", userController.postUser);
-router.put("/users/:userId", userController.putUser);
+router.post("/users", userController.createUser);
+router.put("/users/:userId", userController.updateUser);
 router.delete("/users/:userId", userController.deleteUser);
 
 //comments
-router.get("/comments", commentsController.getComments);
+router.get("/comments", commentsController.getAllComments);
 router.get("/blogs/:blogId/comments", commentsController.getBlogComments);
 router.get("/comments/:commentId", commentsController.getCommentById);
-router.post("/comments", commentsController.postComment);
-router.put("/comments/:commentId", commentsController.putComment);
+router.post("/comments", commentsController.createComment);
+router.put("/comments/:commentId", commentsController.updateComment);
 router.delete("/comments/:commentId", commentsController.deleteComment);
 
 //blogs
-
+router.get("/blogs", blogController.getBlogs);
+router.get("/blogs/:blogId", blogController.getBlogById);
+router.get("/users/:userId/blogs", blogController.getUserBlogs);
+router.post("/blogs", blogController.createBlog);
+router.put("/blogs/:blogId", blogController.updateBlog);
+router.delete("/blogs/:blogId", blogController.deleteBlog);
 
 module.exports = router
