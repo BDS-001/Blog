de06ebec5f60e1f49288575f00fd95b945ab55da
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-01-07 18:38:31 -0500

    add: delete, create, update blog functions

--- Blog_API_Backend/prisma/queries/blogQueries.js
+++ Blog_API_Backend/prisma/queries/blogQueries.js
@@ -1,12 +1,23 @@
 const prisma = require('../prismaClient')
 
+const userSelect = {
+    id: true,
+    email: true,
+    name: true,
+    username: true,
+    createdAt: true,
+    updatedAt: true,
+    roleId: true,
+    role: true
+  };
+
 async function getBlogs(options = {}) {
   try {
     const blogs = await prisma.blog.findMany({
       where: options,
       include: {
         author: {
-          select: { password: false }
+          select: userSelect
         },
       },
       orderBy: { createdAt: 'desc' }
@@ -25,18 +36,18 @@ async function getBlogById(blogId) {
             },
             include: {
                 author: {
-                    select: { password: false }
+                    select: userSelect
                 },
                 comments: {
                     where: {parentId: null},
                     include: {
                         user: {
-                            select: {password: false}
+                            select: userSelect
                         },
                         replies: {
                             include: {
                                 user: {
-                                    select: {password: false}
+                                    select: userSelect
                                 }
                             }
                         }
@@ -50,7 +61,62 @@ async function getBlogById(blogId) {
     }
 }
 
+async function deleteBlog(blogId) {
+    try {
+      const blog = await prisma.blog.delete({
+        where: {
+          id: blogId
+        }
+      });
+      return blog
+    } catch (error) {
+      throw new Error(`Failed to delete blog: ${error.message}`);
+    }
+  }
+
+  async function postBlog(blogData) {
+    try {
+        const blog = await prisma.blog.create({
+            data: blogData,
+            include: {
+                author: {
+                    select: userSelect
+                }
+            }
+        })
+        return blog
+    } catch (error) {
+        throw new Error(`Failed to create blog: ${error.message}`);
+    }
+}
+
+async function putBlog(blogId, newData) {
+    try {
+        const blog = await prisma.blog.update({
+            where: {
+                id: blogId
+            },
+            data: {
+                ...newData,
+                updatedAt: new Date()
+              },
+            include: {
+                comments: true,
+                author: {
+                    select: userSelect
+                }
+            }
+        })
+        return blog
+    } catch (error) {
+        throw new Error(`Failed to update blog: ${error.message}`);
+    }
+}
+
 module.exports = {
     getBlogs,
-    getBlogById
+    getBlogById,
+    deleteBlog,
+    postBlog,
+    putBlog
 }
