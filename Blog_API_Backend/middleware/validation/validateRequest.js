f41d21910a4444ef17ff57e1b5fd5fcb30a9a9a1
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-01-07 23:19:07 -0500

    add: validator files

---
+++ Blog_API_Backend/middleware/validation/validateRequest.js
@@ -0,0 +1,32 @@
+const { validationResult } = require('express-validator');
+const { blogValidators } = require('./validators/blogValidators');
+const { userValidators } = require('./validators/userValidators');
+const { commentValidators } = require('./validators/commentValidators');
+
+
+const validators = {
+  blog: blogValidators,
+  user: userValidators,
+  comment: commentValidators
+};
+
+function validateRequest(resource, operation) {
+    return [
+      // Get all validations rules
+      ...(validators[resource][operation] || []),
+      
+      // Check validation results
+      (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({
+            message: 'Validation error',
+            errors: errors.array()
+          });
+        }
+        next();
+      }
+    ];
+  }
+
+module.exports = {validateRequest}
