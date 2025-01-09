73d272d0ca41641f177f7c29530fec2fc07c8052
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-01-08 20:58:20 -0500

    update: authentication middleware now has user role permission checking

--- Blog_API_Backend/middleware/authentication/authMiddleware.js
+++ Blog_API_Backend/middleware/authentication/authMiddleware.js
@@ -1,16 +1,30 @@
 const passport = require('passport');
 
-function isAuthenticated (req, res, next) {
-    passport.authenticate('jwt', {session: false}, (err, user, info) => {
-        if (err) {
-            return res.status(500).json({error: 'Internal Server Error'})
-        }
+function isAuthenticated(requiredPermissions = []) {
+    return (req, res, next) => {
+        passport.authenticate('jwt', {session: false}, (err, user, info) => {
+            if (err) {
+                return res.status(500).json({error: 'Internal Server Error'})
+            }
 
-        if (!user) {
-            return res.status(401).json({ error: 'Unauthorized - Invalid or missing token' })
-        }
+            if (!user) {
+                return res.status(401).json({ error: 'Unauthorized - Invalid or missing token' })
+            }
 
-        req.user = user
-        return next()
-    })
+            const hasPermissions = requiredPermissions.every(permission => {
+                user.role[permission] === true
+            })
+
+            if (requiredPermissions.length > 0 && !hasPermissions) {
+                return res.status(403).json({ 
+                error: 'Forbidden - Insufficient permissions' 
+                });
+            }
+
+            req.user = user;
+            return next();
+        })
+    }
 }
+
+module.exports = {isAuthenticated}
