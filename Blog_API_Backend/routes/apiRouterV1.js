4cf3abb8482543b31144f7813d70373d05723413
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-01-09 20:03:26 -0500

    update: roputes now use validation middleware

--- Blog_API_Backend/routes/apiRouterV1.js
+++ Blog_API_Backend/routes/apiRouterV1.js
@@ -2,28 +2,30 @@ const router = require("express").Router();
 const userController = require('../controllers/userController')
 const commentsController = require('../controllers/commentController')
 const blogController = require('../controllers/blogController')
+const { validateRequest } = require('../middleware/validateRequest')
+//const { isAuthenticated } = require('../middleware/authMiddleware')
 
-//users
-router.get("/users", userController.getUsers);
-router.get("/users/:userId", userController.getUserById);
-router.post("/users", userController.createUser);
-router.put("/users/:userId", userController.updateUser);
-router.delete("/users/:userId", userController.deleteUser);
+// User Routes
+router.get("/users", validateRequest('param', 'pagination'), validateRequest('param', 'sorting'), userController.getUsers);
+router.get("/users/:userId", validateRequest('param', 'getUserById'), userController.getUserById);
+router.post("/users", validateRequest('user', 'create'), userController.createUser);
+router.put("/users/:userId", validateRequest('param', 'getUserById'), validateRequest('user', 'update'), userController.updateUser);
+router.delete("/users/:userId", validateRequest('param', 'getUserById'), userController.deleteUser);
 
-//comments
-router.get("/comments", commentsController.getAllComments);
-router.get("/blogs/:blogId/comments", commentsController.getBlogComments);
-router.get("/comments/:commentId", commentsController.getCommentById);
-router.post("/comments", commentsController.createComment);
-router.put("/comments/:commentId", commentsController.updateComment);
-router.delete("/comments/:commentId", commentsController.deleteComment);
+// Comment Routes
+router.get("/comments", validateRequest('param', 'pagination'), validateRequest('param', 'sorting'), commentsController.getAllComments);
+router.get("/blogs/:blogId/comments", validateRequest('param', 'getBlogComments'), validateRequest('param', 'pagination'), validateRequest('param', 'sorting'), commentsController.getBlogComments);
+router.get("/comments/:commentId", validateRequest('param', 'getCommentById'), commentsController.getCommentById);
+router.post("/comments", validateRequest('comment', 'create'), commentsController.createComment);
+router.put("/comments/:commentId", validateRequest('param', 'getCommentById'), validateRequest('comment', 'update'), commentsController.updateComment);
+router.delete("/comments/:commentId", validateRequest('param', 'getCommentById'), commentsController.deleteComment);
 
-//blogs
-router.get("/blogs", blogController.getBlogs);
-router.get("/blogs/:blogId", blogController.getBlogById);
-router.get("/users/:userId/blogs", blogController.getUserBlogs);
-router.post("/blogs", blogController.createBlog);
-router.put("/blogs/:blogId", blogController.updateBlog);
-router.delete("/blogs/:blogId", blogController.deleteBlog);
+// Blog Routes
+router.get("/blogs", validateRequest('param', 'pagination'), validateRequest('param', 'sorting'), blogController.getBlogs);
+router.get("/blogs/:blogId", validateRequest('param', 'getBlogById'), blogController.getBlogById);
+router.get("/users/:userId/blogs", validateRequest('param', 'getUserBlogs'), validateRequest('param', 'pagination'), validateRequest('param', 'sorting'), blogController.getUserBlogs);
+router.post("/blogs", validateRequest('blog', 'create'), blogController.createBlog);
+router.put("/blogs/:blogId", validateRequest('param', 'getBlogById'), validateRequest('blog', 'update'), blogController.updateBlog);
+router.delete("/blogs/:blogId", validateRequest('param', 'getBlogById'), blogController.deleteBlog);
 
 module.exports = router
