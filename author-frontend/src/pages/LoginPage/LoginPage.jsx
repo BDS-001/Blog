7dd1eb6d70a4d47cbd5a1fdda479ba3904197392
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-01-16 17:56:33 -0500

    add: simple login form

--- author-frontend/src/pages/LoginPage/LoginPage.jsx
+++ author-frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from '../../assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import styles from './LoginPage.module.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const LoginPage = () => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
 
-export default App
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        return
+    }
+
+    return (
+        <div>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+            <div className={styles.inputContainer}>
+                <div className={styles.formRow}>
+                    <label>Email:</label>
+                    <input type="email" name='email' onChange={handleChange} value={formData.email} placeholder='Enter your email:' />
+                </div>
+                <div className={styles.formRow}>
+                    <label>Password:</label>
+                    <input type="password" name='password' onChange={handleChange} value={formData.password} placeholder='Enter your password' />
+                </div>
+            </div>
+            <button type="submit" >Login</button>
+        </form>
+        </div>
+    );
+    };
+
+export default LoginPage;
